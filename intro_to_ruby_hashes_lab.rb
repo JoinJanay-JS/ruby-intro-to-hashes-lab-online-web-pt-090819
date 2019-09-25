69fc4df7614e99ae576f43b7b3496e76aba34fea
Janay Bailey <tjanay87@gmail.com>
2019-09-25 13:30:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-
+monopoly = {:railroads => ""
 
 end
 
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-:railroads=>
+monopoly = {:railroads=>
   {
     :pieces=>4,
     :rent_in_dollars=>
