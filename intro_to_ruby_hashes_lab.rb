64ee9ef32f0ec1fb5c20f5f725bddd6b52491151
Janay Bailey <tjanay87@gmail.com>
2019-09-25 13:40:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,8 +19,13 @@ monopoly = {:railroads =>
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => 
-{:pieces =>"", :rent_in_dollars => ""} 
+	monopoly = {:pieces => "4",
+    :rent_in_dollars =>
+    {
+      :one_piece_owned =>"25",
+      :two_pieces_owned => "50",
+      :three_pieces_owned => "100",
+      :four_pieces_owned => "200"
 :names => {:readomg_railroads => ""}
 
 
